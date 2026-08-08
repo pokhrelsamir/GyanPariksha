2caa71ac78a2fbc686f863800d2d93171886f967
Samir Pokhrel <poksamir6@gmail.com>
2026-08-08 19:45:08 +0545

    UI JavaScript Code Slight Change

--- js/ui.js
+++ js/ui.js
@@ -498,74 +498,93 @@ const UI = {
 
     showResults() {
 
-        const result =
-            Quiz.getResult();
+    const result =
+        Quiz.getResult();
 
 
-        this.showScreen(
-            this.elements.result
+    /* Save quiz result */
+
+    if (
+        typeof StorageManager !==
+        "undefined"
+    ) {
+
+        StorageManager.saveQuizResult(
+            result
         );
 
-
-        if (this.elements.score) {
-
-            this.elements.score.textContent =
-                result.score;
-
-        }
+    }
 
 
-        if (this.elements.correct) {
+    /* Show result screen */
 
-            this.elements.correct.textContent =
-                result.correct;
-
-        }
-
-
-        if (this.elements.wrong) {
-
-            this.elements.wrong.textContent =
-                result.wrong;
-
-        }
+    this.showScreen(
+        this.elements.result
+    );
 
 
-        if (this.elements.accuracy) {
+    if (this.elements.score) {
 
-            this.elements.accuracy.textContent =
-                `${result.accuracy}%`;
+        this.elements.score.textContent =
+            result.score;
 
-        }
-
-
-        if (this.elements.resultMessage) {
-
-            this.elements.resultMessage.textContent =
-                this.getPerformanceMessage(
-                    result.accuracy
-                );
-
-        }
+    }
 
 
-        this.updateBestScore(
-            result.score
-        );
+    if (this.elements.correct) {
+
+        this.elements.correct.textContent =
+            result.correct;
+
+    }
 
 
-        /* Optional custom event */
+    if (this.elements.wrong) {
 
-        document.dispatchEvent(
-            new CustomEvent(
-                "quizCompleted",
-                {
-                    detail: result
-                }
-            )
-        );
+        this.elements.wrong.textContent =
+            result.wrong;
 
-    },
+    }
+
+
+    if (this.elements.accuracy) {
+
+        this.elements.accuracy.textContent =
+            `${result.accuracy}%`;
+
+    }
+
+
+    if (this.elements.resultMessage) {
+
+        this.elements.resultMessage.textContent =
+            this.getPerformanceMessage(
+                result.accuracy
+            );
+
+    }
+
+
+    if (this.elements.bestScore) {
+
+        this.elements.bestScore.textContent =
+            StorageManager.getBestScore();
+
+    }
+
+
+    document.dispatchEvent(
+
+        new CustomEvent(
+            "quizCompleted",
+            {
+                detail: result
+            }
+        )
+
+    );
+
+}
 
 
     /* --------------------------------------------------
